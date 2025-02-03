93a8df23a37866b8bb920b0fa1c4555424508446
kei1215 <88882972+kei1215@users.noreply.github.com>
2025-02-03 19:24:49 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -112,7 +112,7 @@ def image_view(hash_value):
     response = requests.get(pastebin_url)
     
     if response.status_code == 200:
-            return response
+            return '<img src="{requests}" alt="Uploaded Image">'
     
     return "画像が見つかりません", 404
 
